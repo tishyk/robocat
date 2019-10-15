b45d0e92e740b0b39ad8ca5bde5546c49e1ba92e
Tischenko Sergiy <sytischenko@gmail.com>
2019-10-15 18:37:42 +0300

    Update uploads.py

--- webapp/uploads.py
+++ webapp/uploads.py
@@ -6,16 +6,15 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def upload_file():
-	if request.method == 'POST':
+def upload_file(request):
         # check if the post request has the files part
-		if 'files[]' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		files = request.files.getlist('files[]')
-		for file in files:
-			if file and allowed_file(file.filename):
-				filename = secure_filename(file.filename)
-				file.save(os.path.join(app.config['UPLOAD_DIR'], filename))
-		flash('File(s) successfully uploaded')
-		return redirect('/')
+	if 'files[]' not in request.files:
+		flash('No file part')
+		return False
+	files = request.files.getlist('files[]')
+	for file in files:
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_DIR'], filename))
+	flash('File(s) successfully uploaded')
+	retrun True
